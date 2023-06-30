abe9060e530e54268a5ca3f12047d4757fa80a8f
leecodexxe~ <lee4works@gmail.com>
2023-06-29 18:41:22 -0700

    added the edit functionality

---
+++ views/places/edit.jsx
@@ -0,0 +1,64 @@
+const React = require('react')
+const Def = require('../layout/default')
+
+function edit_form(data) {
+    console.log(data)
+    return (
+        <Def>
+            <form method="POST" action={`/places/${data.id}?_method=PUT`}>
+                <div className="row">
+                    <div className="form-group col-sm-6">
+                        <label htmlFor="name">Place Name</label>
+                        <input 
+                        type='text'
+                        name = "name"
+                        id="name" 
+                        defaultValue={data.place.name}
+                        />
+                    </div>
+                    <div className="form-group col-sm-6">
+                        <label htmlFor="pic">Place Picture</label>
+                        <input 
+                        type='text'
+                        name = "pic"
+                        id="pic" 
+                        defaultValue={data.place.pic}
+                        />
+                    </div>
+                    <div className="form-group col-sm-6">
+                        <label htmlFor="city">City</label>
+                        <input 
+                        type='text'
+                        name = "city"
+                        id="city" 
+                        defaultValue={data.place.city}
+                        />
+                    </div>
+                    <div className="form-group col-sm-6">
+                        <label htmlFor="state">State</label>
+                        <input 
+                        type='text'
+                        name = "state"
+                        id="state" 
+                        defaultValue={data.place.state}
+                        />
+                    </div>
+                    <div className="form-group col-sm-6">
+                        <label htmlFor="cuisines">Cuisines</label>
+                        <input 
+                        type='text'
+                        name = "cuisines"
+                        id="cuisines" 
+                        defaultValue={data.place.cuisines}
+                        />
+                    </div>
+                    <input className="btn btn-primary" type="submit" value="Add Place" />
+                </div>
+
+            </form>
+
+        </Def>
+    )
+}
+
+module.exports = edit_form
---
+++ views/places/show.jsx
@@ -0,0 +1,38 @@
+const React = require("react");
+const Def = require("../layout/default");
+
+function show(data) {
+  return (
+    <Def>
+      <main>
+        <div className="container">
+          <img className="imgOfPlace" src={data.place.pic} alt="" />
+          <div className="nameOfPlace">
+          <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+            Edit
+          </a>
+          <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+            <button type="submit" className="btn btn-danger">
+              Delete
+            </button>
+          </form>
+            <h1>{data.place.name}</h1>
+            <h2>Rating</h2>
+            <h3>Not Rated</h3>
+            <h2>Descroption</h2>
+            <h3>
+              Located in {data.place.city},{data.place.state} and serving{" "}
+              {data.place.cuisines}
+            </h3>
+          </div>
+          <div className="comment">
+            <h2>Comments</h2>
+            <p>No comments Yet!</p>
+          </div>
+        </div>
+      </main>
+    </Def>
+  );
+}
+
+module.exports = show;
